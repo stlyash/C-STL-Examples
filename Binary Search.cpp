3191f3dbe4aaa7271a5c0e8a327a34033b5f5143
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-09-15 08:24:13 +0530

    Update Binary Search.cpp

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,19 +1,38 @@
 #include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
 
 int main()
-{
+{	
+	// binary_search([begin index],[end index],<element to be searched>)
+	
+	// For Vectors
 	
 	vector<int>v={1,2,3,6,8};
-	
-	if(binary_search(v.begin(),v.end(),3))      // binary_search([begin index],[end index],<element to be searched>)
-    	{
+
+	if(binary_search(v.begin(),v.end(),3))      
+    {
 	    cout<<"Element is present"<<endl;
-    	}	
+    }
 	else
 	{
-	    cout<<"element is not present"<<endl;
+	    cout<<"Element is not present"<<endl;
 	}
+	
+	
+	// For Arrays
+	
+	int arr[5] = {1,2,3,4,5};
+	if(binary_search(arr + 0, arr + 4,9))      
+    {
+	    cout<<"Element is present"<<endl;
+    }
+	else
+	{
+	    cout<<"Element is not present"<<endl;
+	}
+
+
 	return 0;
 }
