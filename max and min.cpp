b8b37398a6adb2d4b53ae1ff95751b01613b12e1
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-09-22 10:25:14 +0530

    Update max and min.cpp

--- max and min.cpp
+++ max and min.cpp
@@ -1,15 +1,27 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-	
+int main()
+{
+	// For Vectors
 	vector<int>v={3,1,2,5,6,0};
-	
-auto it= max_element(v.begin(),v.end());
-cout<< *it<<endl;                      //6
 
-auto itr=min_element(v.begin(),v.end());
-cout<< *itr<<endl;                            //0
-	
+	auto it= max_element(v.begin(),v.end());
+	cout<< *it<<endl;                       //returns 6
+
+	auto itr=min_element(v.begin(),v.end());
+	cout<< *itr<<endl;                      //returns 0
+
+	// For Arrays
+	int arr[5] = {1, 2, 3, 4, 5};
+
+	auto itra= max_element(arr+0, arr+5);
+	cout<< *itra<<endl;                     //returns 5
+
+	auto itre=min_element(arr+0, arr+5);
+	cout<< *itre<<endl;                     //returns 1
+
+
 	return 0;
 }
