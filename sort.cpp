fbafef4baceab53f5c408db19f6c1d7f808b5951
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-09-24 07:42:04 +0530

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,21 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
 	
-	vector<int>v={3,1,2,5,6,0};
+	// For vectors
 	
-	sort(v.begin(),v.end());        // ascending order
+	vector<int>v = {3,1,2,5,6,0};
 	
-	for(int i=0;i<v.size();i++)
-	cout<<v[i]<<" ";                   // 0 1 2 3 5 6
+	sort(v.begin(),v.end());                   // ascending order
 	
+	for(int i = 0; i < v.size(); i++)
+	    cout<<v[i]<<" ";                       // returns 0 1 2 3 5 6
 	cout<<endl;
 	
-	sort(v.begin(),v.end(),greater<int>());  // descending order            
+	sort(v.begin(), v.end(), greater<int>());  // descending order            
 	
-		for(int i=0;i<v.size();i++)
-			cout<<v[i]<<" ";
+	for(int i=0;i<v.size();i++)            
+		cout<<v[i]<<" ";                   // returns 6 5 3 2 1 0
+	cout<<endl;
+
+	// For arrays
+
+    	int arr[6] = {3,1,2,5,6,0};
 	
-	return 0;
+	sort(arr + 0, arr + 6);                   // ascending order
+	
+	for(int i=0;i<6;i++)
+	    cout<<arr[i]<<" ";                    // returns 0 1 2 3 5 6
+	cout<<endl;
+	
+	sort(arr + 0, arr + 6, greater<int>() );  // descending order            
+	
+	for(int i = 0; i < 6; i++)
+		cout<<arr[i]<<" ";                // returns 6 5 3 2 1 0
+
+    	return 0;
 }
