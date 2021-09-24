a90f0c5bc5065a485efa3226a4f7b41a8706677d
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-09-24 08:45:43 +0530

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -6,23 +6,22 @@ int main(){
 	
 	queue<int>q;             
 								
-	q.push(4);    // enqueue                              // 4   5    3 
+	q.push(4);                                  
 	q.push(5);
-	q.push(3);
-	q.pop();     //dequeue
+	q.push(3);                          // 4   5    3
+
+	q.pop();                            // 5    3               
 	
-	cout<<q.front()<<endl;                                //4
+	cout<<q.front()<<endl;         // returns 5
 	    
-	cout<<q.size();                           
+	cout<<q.size()<<endl;          // returns 2                        
 	       
-	cout<<q.back();																// 3
-	
+	cout<<q.back()<<endl;	       // returns 3
 	
 	if(q.empty())
 	cout<<"Yes"<<endl;
-	
 	else
-	
 	cout<<"No"<<endl;
-	return 0;
+
+    	return 0;
 }
