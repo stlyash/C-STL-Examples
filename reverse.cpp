270d04ecacefc477ea468a9b41b68e592c6b1153
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-09-24 07:53:23 +0530

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -3,15 +3,24 @@ using namespace std;
 
 int main(){
 	
-	vector<int>v={3,1,2,5,6,0};
+    // For Vectors
+    vector<int>v={3,1,2,5,6,0};
 	
-reverse(v.begin(),v.end());
+    reverse(v.begin(),v.end());
 	
-	for(int i=0;i<v.size();i++)
-	cout<<v[i]<<" ";                  
-	// 0 6 5 2 1 3
-	cout<<endl;
+    for(int i=0;i<v.size();i++)
+    cout<<v[i]<<" ";               // 0 6 5 2 1 3
+    cout<<endl;
+
+    // For Arrays
+    int arr[6] = {3,1,2,5,6,0};
 	
+    reverse(arr + 0, arr + 6);
 	
-	return 0;
+    for(int i=0;i<6;i++)
+    cout<<arr[i]<<" ";             // 0 6 5 2 1 3
+    cout<<endl;	
+	
+    return 0;
+
 }
