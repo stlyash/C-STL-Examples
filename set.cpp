74543154bd73bc8d9f1920d6bf9c61a65399ae45
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-09-24 08:13:47 +0530

    Update set.cpp

--- set.cpp
+++ set.cpp
@@ -11,6 +11,35 @@ int main(){
 	s.insert(2);
 	s.insert(10);
 	
+	set<int>s;                   
+	
+	s.insert(10);
+	s.insert(8);
+	s.insert(1);
+	s.insert(2);
+	s.insert(10);           //  {10,8,1,2} => {1,2,8,10}
+
+        // In C++, sets are sorted automatically
+	
+	cout<<s.size()<<endl;   // 4
+	
+	auto it=s.find(8);
+	if(it!=s.end())
+	cout<<"Present"<<endl;
+	
+	int count=s.count(1);  // return 1
+	
+	cout<<count<<endl;
+	
+	for(auto it=s.begin();it!=s.end();it++)
+	cout<<*it<<endl;
+	
+	s.clear();
+	if(s.empty())
+	cout<<"Yes"<<endl;
+	else
+	cout<<"No"<<endl;
+	
 	cout<<s.size()<<endl;   // 4
 	
 	auto it=s.find(8);
