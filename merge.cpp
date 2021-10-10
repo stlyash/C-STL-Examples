771ee4d9bcc913e20cc39c859a4c1e0fe159879e
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-10-10 18:20:48 +0530

    Update merge.cpp

--- merge.cpp
+++ merge.cpp
@@ -1,22 +1,18 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
 int main(){
 	
-	vector<int>v1={3,1,2,5};
-	vector<int>v2={4,1,8,9};
+	vector<int>v1 = {3,1,2,5};
+	vector<int>v2 = {4,1,8,9};
 	vector<int>v3(10);
 	
 	merge(v1.begin(),v1.end(),v2.begin(),v2.end(),v3.begin());
 	
-	
-	for(int i=0;i<v3.size();i++)
-	cout<<v3[i]<<" ";                  
-	
+	for (int i = 0; i < v3.size(); i++)
+		cout<<v3[i]<<" ";             // returns 3 1 2 4 1 5 8 9 0 0                  
 	cout<<endl;
-	
-
-
 	
 	return 0;
 }
