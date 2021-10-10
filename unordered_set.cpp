25624e975717de955cb4697e256f8934d772545f
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-10-10 17:45:45 +0530

    Update unordered_set.cpp

--- unordered_set.cpp
+++ unordered_set.cpp
@@ -6,29 +6,29 @@ int main(){
 	unordered_set<int>s;                 
 	
 	s.insert(10);
-	s.insert(8);                        // {10,8,1,2}
+	s.insert(8);                        
 	s.insert(1);
 	s.insert(2);
-	s.insert(10);
+	s.insert(10);                      // {10,8,1,2}
 	
-	cout<<s.size()<<endl;   // 4
+	cout<<s.size()<<endl;              // returns 4
 	
-	auto it=s.find(8);   
-	if(it!=s.end())
+	auto it = s.find(8);   
+	if (it != s.end())
 	cout<<"Present"<<endl;
 	
-	int count=s.count(1);  // return 1
+	int count = s.count(1);            // returns 1
 	
 	cout<<count<<endl;
 	
-	for(auto it=s.begin();it!=s.end();it++)
+	for (auto it = s.begin(); it != s.end(); it++)
 	cout<<*it<<endl;
 	
 	s.clear();
-	if(s.empty())
+	if (s.empty())
 	cout<<"Yes"<<endl;
 	else
-	cout<<"No"<<endl;
+	cout<<"No"<<endl;                 // prints Yes
 	
 
 	
