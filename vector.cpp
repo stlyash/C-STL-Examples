ff0772754a207a2deecd07c2c402dd1cce275058
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-10-28 20:17:08 +0530

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -15,7 +15,6 @@ int main(){
 	// simple loop
 	for(int i=0;i<v.size();i++)
 	cout<<v[i]<<" ";
-	
 	cout<<endl;
 	
 	v.pop_back();
@@ -23,17 +22,18 @@ int main(){
 	// iterator
 	for(auto it=v.begin();it!=v.end();it++)
 	cout<<*it<<" ";
-	
 	cout<<endl;
 	
 	// taking user input
-	
 	int n;
 	cin>>n;
 	vector<int>v1(n,0);
 	for(int i=0;i<n;i++)
 	cin>>v[i];
 	
+	// Erasing
+	v.erase(v.begin()+4);              // erases the fifth element of the vector v
+	v.erase(v.begin()+2,v.begin()+5);  // erases all the elements from the third element to the fifth element.
 	
 	return 0;
 }
