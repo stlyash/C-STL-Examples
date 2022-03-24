a6f975b5b21550fe7899f6559b9a1b2bd233724a
Yashasvi <66861659+stlyash@users.noreply.github.com>
2022-03-24 21:09:45 +0530

    Update multiset.cpp

--- multiset.cpp
+++ multiset.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
 int main(){
@@ -6,30 +7,30 @@ int main(){
 	multiset<int>s;                  
 	
 	s.insert(10);
-	s.insert(8);                   // {1,2,8,10,10} 
+	s.insert(8);                   
 	s.insert(1);
 	s.insert(2);
-	s.insert(10);
+	s.insert(10);  // {1,2,8,10,10} 
 	
 	cout<<s.size()<<endl;   // 5  
 	
 	auto it=s.find(8);
 	if(it!=s.end())
-	cout<<"Present"<<endl;
+	    cout<<"Present"<<endl;
 	
-	int count=s.count(10);   // 2
+	int count=s.count(10);
+    cout<<count<<endl;  // 2
 	
-	s.erase(10);                   // {1,2,8}
-	
-	cout<<count<<endl;
+	s.erase(10);                   
 	
 	for(auto it=s.begin();it!=s.end();it++)
-	cout<<*it<<endl;
+	    cout<<*it<<endl;  // {1,2,8}
 	
 	s.clear();
+
 	if(s.empty())
-	cout<<"Yes"<<endl;
+	    cout<<"Yes"<<endl;
 	else
-	cout<<"No"<<endl;
+	    cout<<"No"<<endl;  // Yes
 	
 }
