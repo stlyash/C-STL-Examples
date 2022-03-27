cc8b656ed84314885cf454ec386bcb6a199ff01b
Yashasvi <66861659+stlyash@users.noreply.github.com>
2022-03-27 21:36:48 +0530

    Create deque.cpp

---
+++ deque.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    deque<int> d = {2, 3, 4};
+    
+    d.push_front(1);
+    d.pop_back(9);
+    
+    for(int x: d)
+        cout<< x <<" ";
+    cout<<endl;
+    
+    d.push_back(3);
+    d.pop_front(9);
+    
+    for(int x: d)
+        cout<< x <<" ";
+    cout<<endl;
+    
