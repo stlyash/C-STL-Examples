e83a55530704a49860906603a4a195b5521773cf
Yashasvi <66861659+stlyash@users.noreply.github.com>
2022-08-11 09:06:17 +0530

    Update and rename csb.cpp to popCount.cpp

---
+++ popCount.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+    int n=10;
+	
+    // Displaying the number of bits which are set to true 
+    cout<< __builtin_popcount(n) <<endl; // 2
+	
+    //(in binary notation 10 => 1 0 1 0 => 2 bits are set to true)
+}
