749008eb904b74ce220f9974be960051449dc498
Yashasvi <kmryashasvi@gmail.com>
2023-06-10 19:21:47 +0530

    added comments for explanations

--- list.cpp
+++ list.cpp
@@ -5,30 +5,35 @@ int main(){
 
 	// List in C++ = Doubly Linked List
 	
-	list<int>l3={6,6,4,5,6,8};
-	cout<<l3.front()<<" "<<l3.back()<<endl;   // 4 8
+	// Declaring a list named 'l3' and initialize it with values
+	list<int> l3 = {6, 6, 4, 5, 6, 8};
+
+	// Printing the first and last elements of the list
+	cout << l3.front() << " " << l3.back() << endl;   // 4 8
 	
 	// Functions to push integers in front and back
 	l3.push_front(9);   // 9 4 5 6 8
 	l3.push_back(6);    // 9 4 5 6 8 6
 
-	// Removing all occurances of specified digit
+	// Removing all occurrences of the specified digit
 	l3.remove(6);       // 9 4 5 8
 
-	for(auto it=l3.begin();it!=l3.end();it++)
-	cout<<*it<<" ";   // 9 4 5 8
-	cout<<endl;
+	// Printing the elements of the list using a loop and iterator
+	for(auto it = l3.begin(); it != l3.end(); it++)
+		cout << *it << " ";   // 9 4 5 8
+	cout << endl;
 
-	// Emplace functions can also be used
+	// Emplace functions can also be used to insert elements
 	l3.emplace_front(14);
 	l3.emplace_back(16);
-	for(auto it=l3.begin();it!=l3.end();it++)
-	cout<<*it<<" ";   // 14 9 4 5 8 16
-	cout<<endl;
+	for(auto it = l3.begin(); it != l3.end(); it++)
+		cout << *it << " ";   // 14 9 4 5 8 16
+	cout << endl;
 
+	// Popping the first and last elements of the list
 	l3.pop_front();
 	l3.pop_back();
-	for(auto it=l3.begin();it!=l3.end();it++)
-	cout<<*it<<" ";   // 9 4 5 8
-	cout<<endl;
+	for(auto it = l3.begin(); it != l3.end(); it++)
+		cout << *it << " ";   // 9 4 5 8
+	cout << endl;
 }
